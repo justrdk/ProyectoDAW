be13d6095aca370d818a39f064a450759fdaaf70
Osman Hernandez <osman.sist@gmail.com>
2012-12-01 13:06:20 -0600

    borrado

--- server.js
+++
@@ -1,9 +0,0 @@
-var http = require("http");
-var area = require("./funcionesExternas");
-var temp2 = area.calcularArea(2,2);
-
-http.createServer(function(request,response){
-	response.writeHead(200,{"Content-Type": "text/plain"});
-	response.write("El area es: " + area.calcularArea(2,2));
-	response.end();
-}).listen(8888);
